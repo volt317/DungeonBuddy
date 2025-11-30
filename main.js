64a88993e37140f597139296fb80c65feaa47078
volt <volt@volt-ThinkCentre-M80q>
2025-11-30 16:37:20 -0500

    js npm rebuild

--- main.js
+++ main.js
@@ -229,8 +229,8 @@ var DungeonBuddy = /** @class */ (function (_super) {
         for (var _i = 0, _a = Object.entries(metadata); _i < _a.length; _i++) {
             var _b = _a[_i], key = _b[0], value = _b[1];
             // Quote strings with special characters or spaces
-            if (typeof value === "string" && /[:"'#]/.test(value)) {
-                lines.push("".concat(key, ": \"").concat(value.replace(/"/g, '\\"'), "\""));
+            if (typeof value === "string") {
+                lines.push("".concat(key, ": ").concat(JSON.stringify(value)));
             }
             else {
                 lines.push("".concat(key, ": ").concat(value));
